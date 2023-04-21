151cada636886dd48a8a81c746feb07b0a8eba83
omwami <ianalpha@protonmail.com>
2023-04-21 18:26:46 +0300

    bubble sort - array

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,7 +19,7 @@ void bubble_sort(int *array, size_t size)
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-				print_array((const int*)array, size);
+				print_array((const int *)array, size);
 			}
 		}
 	}
