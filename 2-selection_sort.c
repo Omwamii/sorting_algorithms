6317b904345cdb7f330358ac43848c4b928526f6
omwami <ianalpha@protonmail.com>
2023-04-22 16:25:40 +0300

    selection sort: array

---
+++ 2-selection_sort.c
@@ -0,0 +1,8 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts array with selection sort
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size);
