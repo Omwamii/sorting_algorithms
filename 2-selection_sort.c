e3fb52e709c83e96153bfd7d81fb8a7b74359630
omwami <ianalpha@protonmail.com>
2023-04-22 17:05:37 +0300

    selection sort: array

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,8 +1,43 @@
 #include "sort.h"
 
+void swap(int *x, int *y);
 /**
  * selection_sort - sorts array with selection sort
  * @array: array to be sorted
  * @size: size of the array
  */
-void selection_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+				min = j;
+		}
+		if (min != i)
+		{
+			swap(&array[min], &array[i]);
+			print_array((const int *)array, size);
+		}
+
+	}
+}
+
+/**
+ * swap - swaps items in array
+ * @x: first item to swap
+ * @y: item 2
+ * @array: array to be printed after swap
+ * @size: size of array
+ */
+void swap(int *x, int *y)
+{
+	int tmp = *x;
+
+	*x = *y;
+	*y = tmp;
+}
