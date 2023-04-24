ad57d7bbb838de07071172d985a8f1eef2b7cabf
omwami <ianalpha@protonmail.com>
2023-04-24 16:37:13 +0300

    bubble sort: array

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,7 +10,7 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 	size_t i, j;
 
-	if (array == NULL || size <= 0)
+	if (array == NULL || size < 2)
 		return;
 	for (i = 0; i < size; i++)
 	{
