ffcd5737b1e0734ac896ead9b9d1dfc73d13102f
omwami <ianalpha@protonmail.com>
2023-04-24 13:29:22 +0300

    counting sort: array

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -19,16 +19,21 @@ void counting_sort(int *array, size_t size)
 			k  = array[i];
 	}
 	tmp1 = malloc((k + 1) * sizeof(int));
-	tmp2 = malloc(size * sizeof(int));
-	if (tmp1 == NULL || tmp2 == NULL)
+	if (tmp1 == NULL)
 		return;
+	tmp2 = malloc(size * sizeof(int));
+	if (tmp2 == NULL)
+	{
+		free(tmp1);
+		return;
+	}
 	for (i = 0; i < (k + 1); i++) /* set all values to 0 */
 		tmp1[i] = 0;
 	for (i = 0; i < len; i++) /* increment tmp1 value count */
 		++(tmp1[array[i]]);
-	print_array(tmp1, k + 1);
 	for (i = 1; i <= k; i++) /* increment values in tmp1 to match indices */
 		tmp1[i] = tmp1[i] + tmp1[i - 1];
+	print_array(tmp1, k + 1);
 	for (i = len - 1; i >= 0; i--)
 		tmp2[--(tmp1[array[i]])] = array[i];
 	for (i = 0; i < len; i++)
